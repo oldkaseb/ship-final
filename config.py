727b060283d3f32ea7bcf8bba2c51e014d6770da
oldkaseb <hamedsocialworks@gmail.com>
2025-08-03 22:57:28 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 import os
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_ID = os.getenv('ADMIN_ID')
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+CHANNEL_ID = os.getenv("CHANNEL_ID")  # بدون @
+DATA_FOLDER = "data"
