3f3f48441b8ec8a81174270ad735b982bda1257f
oldkaseb <hamedsocialworks@gmail.com>
2025-08-04 00:14:33 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-CHANNEL_ID = os.getenv("CHANNEL_ID")  # بدون @
-DATA_FOLDER = "data"
+# توکن ربات از متغیر محیطی
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+# آیدی سازنده اصلی برای کنترل ربات
+ADMIN_ID = int(os.getenv('ADMIN_ID')) if os.getenv('ADMIN_ID') else None
+
+# آیدی کانال برای اجبار عضویت
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")  # مثل: @mychannel
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")  # لینک مستقیم
+
+# نام ربات برای پیام‌های دکمه‌ها یا اعلان‌ها
+BOT_NAME = "فضول گروه"
+
+# آیدی تیم سازنده (در پیام‌های اطلاع‌رسانی استفاده می‌شود)
+CREATOR_TAG = "@oldkaseb"
+TEAM_NAME = "Souls"
