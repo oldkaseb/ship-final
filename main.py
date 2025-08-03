d0e5eea1af5818b5132a65ac126a721b81857c84
oldkaseb <hamedsocialworks@gmail.com>
2025-08-04 02:18:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-# main.py - Entry Point for شیپر فضوله
+import asyncio
+from loader import dp, bot
 from handlers.start import run
+from aiogram import executor
 
-if __name__ == '__main__':
-    run()
+# ثبت تمام هندلرها
+run()
+
+async def on_startup(dispatcher):
+    print("ربات با موفقیت راه‌اندازی شد.")
+
+async def main():
+    await on_startup(dp)
+    await dp.start_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
