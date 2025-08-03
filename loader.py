06ff05aaeb75e521b6b4641d7f254b786014a202
oldkaseb <hamedsocialworks@gmail.com>
2025-08-04 01:25:07 +0400

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,12 +1,7 @@
-# loader.py - بارگذاری اولیه Bot و Dispatcher
+# loader.py
 
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import BOT_TOKEN
 
-# حافظه موقت برای ذخیره داده‌ها (مثلاً برای مکالمات مرحله‌ای)
-storage = MemoryStorage()
-
-# ساخت نمونه‌های Bot و Dispatcher
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot)
